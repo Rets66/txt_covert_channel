dac84a4bdb28cd8a4c8422e0a35a5cd938ee8a81
Rets66 <asce_salut666chere@icloud.com>
2019-05-20 22:31:11 +0900

    Add docstring

--- server/fakedns.py
+++ server/fakedns.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
-import socket
+import socketserver
 
 PORT=9999
 HOST='localhost'
-RTYPE_TXT = 16
 
-class EventHandler()
+# -- Resource Record --
+RTYPE_A=1
+RTYPE_CNAME=5
+RTYPE_MX=15
+RTYPE_TXT=16
+RTYPE_AAAA=28
+
+
+class EventHandler(socketserver.BaseRequestHandler):
     """
     - Listen
     - Extract text file
@@ -15,16 +22,26 @@ class EventHandler()
     - Input TXT the text
     """
 
-class DNSHandler(socketserver.BaseRequestHandler):
+    def __init__(self):
+        pass
 
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
 
 class CreateContent():
+    """
+    - Extract a line from command.txt
+    - Encode the line by some encoding methods such as Base64, etc
+    - Put on the prefix to the encoded lien
+    """
+
+    def __init__(self):
+        pass
 
     def get_file():
+        pass
 
 
 if __name__ == '__main__':
-    
+    pass
