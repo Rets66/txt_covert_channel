e37555aec56301a434feeca1e12c79fb8145bf48
Rets66 <asce_salut666chere@icloud.com>
2019-05-20 22:15:14 +0900

    Modify class comment

--- server/fakedns.py
+++ server/fakedns.py
@@ -3,7 +3,7 @@
 import socket
 
 PORT=9999
-HOST=
+HOST='localhost'
 RTYPE_TXT = 16
 
 class EventHandler()
@@ -16,21 +16,12 @@ class EventHandler()
     """
 
 class DNSHandler(socketserver.BaseRequestHandler):
-    """
-    The request handler class for DNS server.
-    """
 
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
 
 class CreateContent():
-    """
-    - command.txtから適当に行を抽出する
-    - エンコードした文字列をプレフィックスをアドオンして，送信する
-    * 正規のドメイン検証すると，面倒なことになりそうということであれば，
-      有名ラベルを利用することで擬似ドメイン検証を作成することができるのはないか
-    """
 
     def get_file():
 
