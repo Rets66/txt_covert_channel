6233dcdc5a0fdcddb2888ac94e867fa64992c227
Rets66 <asce_salut666chere@icloud.com>
2019-05-20 19:53:32 +0900

    Create labels.txt

---
+++ server/fakedns.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import socket
+
+PORT=9999
+HOST=
+RTYPE_TXT = 16
+
+class EventHandler()
+    """
+    - Listen
+    - Extract text file
+    - Encode text
+    - Add prefix
+    - Input TXT the text
+    """
+
+class DNSHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for DNS server.
+    """
+
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+
+class CreateContent():
+    """
+    - command.txtから適当に行を抽出する
+    - エンコードした文字列をプレフィックスをアドオンして，送信する
+    * 正規のドメイン検証すると，面倒なことになりそうということであれば，
+      有名ラベルを利用することで擬似ドメイン検証を作成することができるのはないか
+    """
+
+    def get_file():
+
+
+if __name__ == '__main__':
+    
--- server/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-def example():
-    pass
-
-if __name__ == '__main__':
-    main()
